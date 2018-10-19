3eaa451e6951a0433bbf37d22bd3cf8f70b2affb
liqiangwaini <550163812@qq.com>
2018-10-19 10:18:43 +0800

    init helloword respo

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String temp = null;
+		
+		if ("aaa".equals(temp)) {
+			System.out.print("ok");
+		} else {
+			System.out.print("error");
+		}
+	}
+
+}
